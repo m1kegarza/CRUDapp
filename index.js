d3d65916e48abf0e20a95b402fa0eaaa71f469ec
m1kegarza <miguelgarzanarvaez@gmail.com>
2023-01-07 19:42:11 -0600

    delete function

--- index.js
+++ index.js
@@ -30,79 +30,28 @@ document.getElementById("timetxt").value = '';
 render()
 }
 
-function borrar(){
-    localStorage.removeItem(getElementById)
-    render()
-}
-
 function render(){
     console.log(localStorage.getItem('reminders'));
     let lista = document.getElementById("lista");
     let datos = JSON.parse(localStorage.getItem("reminders")) ? JSON.parse(localStorage.getItem("reminders")) : []
     console.log(datos)
     lista.innerHTML = ''
-    datos.forEach(element => {
-        console.log(element)
+    datos.forEach((element, index) => {
+        console.log(index)
         lista.innerHTML += `
         <p> ${element.desc} </p>
         <p> ${element.fecha} </p>
         <p> ${element.hora} </p>
-        <button id="Borrar" onclick="borrar()">Borrar</button>
+        <button id="Borrar" onclick="borrar(${index})">Borrar</button>
         `
     });
 }
 
-render()
-
-
-
-
-
-/*function guardar(){
-    //Capturar los datos de los inputs
-  let nom = document.getElementById("nombretxt").value;
-  let apelli = document.getElementById("apellidotxt").value;
-  let age = document.getElementById("edadtxt").value;
-
-   // Fuardamos los valores en una variable
-  let inputValues = ({
-    "nombre": nom,
-    "apellido": apelli,
-    "edad": age
-  })*/
-
-//Guardar los datos en un array
-//let datos = new Array(); -> aunque no es tan necesario
-/* Recordemos que localstorage obtiene los valores como cadenas de texto por lo que el JSON.parse convierte las cadenas de texto en 
-  JSON (objetos) -> A su vez, añadimos el operador ternario bajo la sintaxis : condicional ? expresion1 : expresion2 */
-/* let datos = JSON.parse(localStorage.getItem('usuarios')) ? JSON.parse(localStorage.getItem('usuarios')) : []
-  console.log(inputValues)
-  datos.push(inputValues)
-  console.log(datos)
-  // Una vez obteniendo los objetos , los convertiremos a strings
- console.log(JSON.stringify(datos))
- localStorage.setItem('usuarios', JSON.stringify(datos))
-//Guardar los datos en el localstorage*/
-
-/*//borrar los elementos del formulario
-document.getElementById("nombretxt").value = '' ;
-document.getElementById("apellidotxt").value = ''; 
-document.getElementById("edadtxt").value = ''; 
-render()
+function borrar(position){
+    let datos = JSON.parse(localStorage.getItem("reminders")) ? JSON.parse(localStorage.getItem("reminders")) : []
+    datos.splice(position, 1)
+    localStorage.setItem('datos', datos)
+    render()
 }
 
-/*function render(){
-    console.log(localStorage.getItem('usuarios'));
-    let lista = document.getElementById("lista");
-    let datos = JSON.parse(localStorage.getItem("usuarios")) ? JSON.parse(localStorage.getItem("usuarios")) : []
-    console.log(datos)
-    lista.innerHTML = ''
-    datos.forEach(element => {
-        console.log(element)
-        lista.innerHTML += `
-        <p> ${element.nombre} </p>
-        <p> ${element.apellido} </p>
-        <p> ${element.edad} </p>
-        `
-    });
-}*/
+render()
