2b90dc3cafd58ca3fac8e4524f87454a00a9099e
m1kegarza <miguelgarzanarvaez@gmail.com>
2023-01-07 18:41:30 -0600

    code integration 1st try

--- index.js
+++ index.js
@@ -11,11 +11,41 @@ function add(){
     "time": time
   })
 
+let datos = JSON.parse(localStorage.getItem('reminders')) ? JSON.parse(localStorage.getItem('reminders')) : []
+console.log(inputValues)
+datos.push(inputValues)
+console.log(datos)
+  // Una vez obteniendo los objetos , los convertiremos a strings
+ console.log(JSON.stringify(datos))
+ localStorage.setItem('reminders', JSON.stringify(datos))
+//Guardar los datos en el localstorage
 
+//borrar los elementos del formulario
+document.getElementById("desctxt").value = '' ;
+document.getElementById("datetxt").value = ''; 
+document.getElementById("timetxt").value = ''; 
+render()
+}
 
+function render(){
+    console.log(localStorage.getItem('reminders'));
+    let lista = document.getElementById("lista");
+    let datos = JSON.parse(localStorage.getItem("reminders")) ? JSON.parse(localStorage.getItem("reminders")) : []
+    console.log(datos)
+    lista.innerHTML = ''
+    datos.forEach(element => {
+        console.log(element)
+        lista.innerHTML += `
+        <p> ${element.desc} </p>
+        <p> ${element.date} </p>
+        <p> ${element.time} </p>
+        `
+    });
+}
 
+render()
 
-function guardar(){
+/*function guardar(){
     //Capturar los datos de los inputs
   let nom = document.getElementById("nombretxt").value;
   let apelli = document.getElementById("apellidotxt").value;
@@ -26,21 +56,22 @@ function guardar(){
     "nombre": nom,
     "apellido": apelli,
     "edad": age
-  })
+  })*/
 
   //Guardar los datos en un array 
   //let datos = new Array(); -> aunque no es tan necesario
-  // Recordemos que localstorage obtiene los valores como cadenas de texto por lo que el JSON.parse convierte las cadenas de texto en JSON (objetos) -> A su vez, añadimos el operador ternario bajo la sintaxis : condicional ? expresion1 : expresion2 
-  let datos = JSON.parse(localStorage.getItem('usuarios')) ? JSON.parse(localStorage.getItem('usuarios')) : []
+  /* Recordemos que localstorage obtiene los valores como cadenas de texto por lo que el JSON.parse convierte las cadenas de texto en 
+  JSON (objetos) -> A su vez, añadimos el operador ternario bajo la sintaxis : condicional ? expresion1 : expresion2 */
+ /* let datos = JSON.parse(localStorage.getItem('usuarios')) ? JSON.parse(localStorage.getItem('usuarios')) : []
   console.log(inputValues)
   datos.push(inputValues)
   console.log(datos)
   // Una vez obteniendo los objetos , los convertiremos a strings
  console.log(JSON.stringify(datos))
  localStorage.setItem('usuarios', JSON.stringify(datos))
-//Guardar los datos en el localstorage
+//Guardar los datos en el localstorage*/
 
-//borrar los elementos del formulario
+/*//borrar los elementos del formulario
 document.getElementById("nombretxt").value = '' ;
 document.getElementById("apellidotxt").value = ''; 
 document.getElementById("edadtxt").value = ''; 
@@ -61,6 +92,6 @@ function render(){
         <p> ${element.edad} </p>
         `
     });
-}}
+}
 
-render()
+render()*/
